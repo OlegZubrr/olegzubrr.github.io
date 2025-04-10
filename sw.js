8c1133a4dd247e95d2a39e0162598e0c24d9c9ef
Oleg Zubr <olegzubr769@gmail.com>
2025-04-10 11:41:57 +0300

    midifi media

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-const CACHE_NAME = "v7"; // Обновите версию кэша
+const CACHE_NAME = "v8";
 
 self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return Promise.all(
-                    cacheNames.map((cache) => {
-                        if (cache !== CACHE_NAME) {
-                            return caches.delete(cache);
-                        }
-                    })
-                );
-            })
-            .then(() => self.clients.claim()) // Активируем сразу для всех клиентов
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
     );
 });
 
@@ -23,31 +20,18 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
     const url = new URL(event.request.url);
-
     if (url.pathname.startsWith("/source/")) {
         event.respondWith(
-            caches.open(CACHE_NAME).then(async (cache) => {
-                try {
-                    // Первым делом пытаемся получить свежие данные из сети
-                    const fetchResponse = await fetch(event.request);
-
-                    // Проверяем валидность ответа
-                    if (!fetchResponse || fetchResponse.status !== 200) {
-                        return fetchResponse;
-                    }
-
-                    // Клонируем ответ для кэширования
-                    const responseToCache = fetchResponse.clone();
-                    cache.put(event.request, responseToCache);
-
-                    return fetchResponse;
-                } catch (error) {
-                    // Если сеть недоступна - ищем в кэше
-                    const cachedResponse = await cache.match(event.request);
+            caches.open(CACHE_NAME).then((cache) => {
+                return cache.match(event.request).then((response) => {
                     return (
-                        cachedResponse || new Response(null, { status: 404 })
+                        response ||
+                        fetch(event.request).then((fetchResponse) => {
+                            cache.put(event.request, fetchResponse.clone());
+                            return fetchResponse;
+                        })
                     );
-                }
+                });
             })
         );
     } else {
